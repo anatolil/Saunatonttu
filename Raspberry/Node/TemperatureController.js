04993d3bddb9d1c4ca71d5e8d84d26b45569005c
Kasper Kiiskinen <kisperkkk@hotmail.com>
2015-11-18 17:44:56 +0200

    Lets read temp from file

--- Raspberry/Node/TemperatureController.js
+++ Raspberry/Node/TemperatureController.js
@@ -1,22 +1,10 @@
-var wyliodrin = require("wyliodrin");
+var fs = require('fs');
 
 var route;
-
-var grove;
-var B;
 
 function TemperatureController()
 {
     route = "/get/temperature";
-    
-    if (process.env.wyliodrin_board == "raspberrypi") { 
-        grove = 300; 
-        wyliodrin.grovepiSetup (grove, 4); 
-    } else { 
-        grove = 0; 
-    }
-
-    B = 3975;                  //B value of the thermistor
 }
 
 TemperatureController.prototype.getRoute = function()
@@ -27,15 +15,15 @@ TemperatureController.prototype.getRoute = function()
 TemperatureController.prototype.action = function(response, json, query)
 {
     response.writeHead(200, {'Content-Type': 'application/json'});
-    var jsonResponse = JSON.stringify({ temperature: getTemperature (wyliodrin.analogRead (grove+1))});
+    var jsonResponse = JSON.stringify({ temperature: getTemperature()});
     response.end(jsonResponse);
 }
 
-function getTemperature (value){
-  if (value==0) value=0.0001;
-  value =(1023-value)*10000/value;
-  var celsius=Math.round((1/(Math.log(value/10000.0)/B+1/298.15)-273.15)*100)/100;
-  return celsius;
+function getTemperature (){
+  fs.readFile('/run/saunatonttu/temp.txt', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+  });
 }
 
 module.exports = TemperatureController;
