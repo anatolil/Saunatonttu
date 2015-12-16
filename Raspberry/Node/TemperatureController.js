7804add484f92073495a58d7640e9878e0e60185
Susikettu <zx3end@gmail.com>
2015-12-16 15:34:37 +0200

    showtemperature.html and TemperatureController additions

--- Raspberry/Node/TemperatureController.js
+++ Raspberry/Node/TemperatureController.js
@@ -1,10 +1,12 @@
 var fs = require('fs');
 
+var loadHtml = require('./HtmlLoader.js');
+
 var route;
 
 function TemperatureController()
 {
-    route = {"/get/temperature": TemperatureController.prototype.renderTemperature};
+    route = {"/get/temperature": TemperatureController.prototype.renderTemperature, "/get/temperatureadjust": TemperatureController.prototype.adjustTemperature};
 }
 
 TemperatureController.prototype.getRoute = function()
@@ -17,6 +19,12 @@ TemperatureController.prototype.renderTemperature = function (response, json, qu
     response.writeHead(200, {'Content-Type': 'application/json'});
     var jsonResponse = JSON.stringify({ temperature: fs.readFileSync('temperature.txt', 'utf8') });
     response.end(jsonResponse);
+}
+
+TemperatureController.prototype.adjustTemperature = function (response, json, query)
+{
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    loadHtml(response, './HTML/showtemperature.html', null);
 }
 
 module.exports = TemperatureController;
