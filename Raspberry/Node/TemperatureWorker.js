d929cc415e929f928fed1699d1e92da4cd23cc97
Juho Vanhanen <juho@vanhanen.io>
2015-12-16 12:03:16 +0200

    Fixes

--- Raspberry/Node/TemperatureWorker.js
+++ Raspberry/Node/TemperatureWorker.js
@@ -14,19 +14,23 @@ serialport.open(function (error) {
         console.log('Serial port error');
     }
     else {
+        wyliodrin.pinMode(0, 1);
         serialport.on('data', function (data) {
             if (data.match(/(\d{2}\.\d{2})/)) {
                 temperature = data.trim()
-                if (Math.abs(temperature - currentTemperature) < 3) {
+                if (Math.abs(temperature - currentTemperature) < 5 || !currentTemperature) {
                     currentTemperature = temperature;
                     fs.writeFileSync('temperature.txt', temperature, 'utf8');
                     
                     temperatureGoal = fs.readFileSync('temperatureGoal.txt', 'utf8');
+                    console.log('Current: ' + temperature + ' Goal:' + temperatureGoal);
                     if (temperature > temperatureGoal + 5) {
-                        wyliodrin.pinMode (0, 0);
+                        wyliodrin.digitalWrite(0, 0);
+                        console.log('Relay off');
                     }
                     else if (temperature < temperatureGoal - 5) {
-                        wyliodrin.pinMode (0, 1);
+                        wyliodrin.digitalWrite(0, 1);
+                        console.log('Relay on');
                     }
                 }
             }
