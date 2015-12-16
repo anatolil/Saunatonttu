dd59fceea8531622e4e03847d03dcb106119abf2
Kasper Kiiskinen <kisperkkk@hotmail.com>
2015-12-16 12:03:13 +0200

    Calenteriin voi lisata paivia

--- Raspberry/Node/test.js
+++ Raspberry/Node/test.js
@@ -1,10 +1,19 @@
 var loadHtml = require('./HtmlLoader.js');
 
 var route;
+var events = new Array();
+
+function Event(title, start, end) 
+{
+    this.title = title;
+    this.start = start;
+    this.end = end;
+    this.overlap = false;
+}
 
 function TestController()
 {
-    route = { "/get/test": TestController.prototype.showTime, "/get/getEvents": TestController.prototype.getJson };
+    route = { "/get/test": TestController.prototype.showTime, "/get/getEvents": TestController.prototype.getJson, "/post/saveEvent": TestController.prototype.saveEvent };
 }
 
 TestController.prototype.getRoute = function()
@@ -21,23 +30,17 @@ TestController.prototype.showTime = function(response, json, query)
 TestController.prototype.getJson = function(response, json, query)
 {
     response.writeHead(200, {'Content-Type': 'application/json'});
-    response.end(JSON.stringify(getUnixtimeJson()));
+    response.end(getEventsJson());
 }
 
-function Item(title, start, end) {
-    this.title = title;
-    this.start = start;
-    this.end = end;
-}
-
-function getUnixtimeJson()
+TestController.prototype.saveEvent = function(response, json, query)
 {
-    var asd = new Item("Kisper", "2015-12-12T10:30:00-05:00", "2015-12-12T12:30:00-05:00");
-    var asd2 = new Item("Kispers", "2015-12-15T10:30:00-05:00", "2015-12-15T12:30:00-05:00");
-    var lol = new Array();
-    lol.push(asd);
-    lol.push(asd2);
-    return JSON.stringify(lol);
+    events.push(new Event(json['title'], json['start'], json['end']));
+}
+
+function getEventsJson()
+{
+    return JSON.stringify(events);
 }
 
 module.exports = TestController;
